82b4d8cca32cde1cef7379846cee33f106b2bd16
Revaldi Dwi Octavian <revaldidwioctavian@gmail.com>
2022-06-16 10:18:58 +0700

    Pembuatan halaman profil

--- resources/views/client/Pages/profile.blade.php
+++ resources/views/client/Pages/profile.blade.php
@@ -1,9 +1,116 @@
 @extends('layouts.client.app')
 
 @section('content')
-<div class="py-7">
-    <h1>Hello World!</h1>
+<header class="header-2">
+  <div class="page-header min-vh-75 relative" style="background-image: url('{{asset('dist/img/client/bg.jpg')}}')">
+    <span class="mask bg-gradient-dark opacity-4"></span>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7 text-center mx-auto">
+          <h1 class="text-white pt-3 mt-n5">Rukun Warga 16</h1>
+          <p class="lead text-white mt-3">Profil RW <br/> ------- </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<div class="card card-body blur shadow-blur mx-3 mx-md-4 mt-n6">
+
+    <section class="pt-7">
+        <div class="container">
+            <h1 class="mb-5">Kegiatan kami</h1>
+            <div class="row py-4">
+                <div class="col-lg my-auto py-3">
+                    <h3>Tentang Kami</h3>
+                    <p class="pe-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat est facilis
+                        eligendi! Omnis repellendus sapiente at aperiam earum deleniti error fuga accusantium voluptate
+                        esse. Magnam sit earum fugit veritatis molestiae.</p>
+                    <a href="javascript:;" class="text-primary icon-move-right">Baca Selengkapnya
+                        <i class="fas fa-arrow-right text-sm ms-1"></i>
+                    </a>
+                </div>
+                <div class="col-lg my-auto py-4">
+                    <img class="img-fluid" src="{{asset('dist/img/client/bg.jpg')}}" alt="Kegiatan">
+                </div>
+            </div>
+
+            <div class="row py-4">
+                <div class="col-lg-5 my-auto py-4">
+                    <img class="img-fluid" src="{{asset('dist/img/client/bg.jpg')}}" alt="Kegiatan">
+                </div>
+                <div class="col-lg-7 my-auto py-3">
+                    <h3>Tentang Kami</h3>
+                    <p class="pe-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat est facilis
+                        eligendi! Omnis repellendus sapiente at aperiam earum deleniti error fuga accusantium voluptate
+                        esse. Magnam sit earum fugit veritatis molestiae.</p>
+                    <a href="javascript:;" class="text-primary icon-move-right">Baca Selengkapnya
+                        <i class="fas fa-arrow-right text-sm ms-1"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+  <!-- -------- START Features w/ icons and text on left & gradient title and text on right -------- -->
+  <div class="container py-6">
+  <div class="row">
+    <div class="col-lg-6 my-auto">
+      <h3 class="mt-5 mt-lg-0">Read More About Us</h3>
+      <p class="pe-5">Pain is what we go through as we become older. We get insulted by others, lose trust for those others. We get back stabbed by friends. It becomes harder for us to give others a hand.</p>
+      <a href="javascript:;" class="text-primary icon-move-right">More about us
+        <i class="fas fa-arrow-right text-sm ms-1"></i>
+      </a>
+    </div>
+    <div class="col-lg-6 mt-lg-0 mt-5 ps-lg-0 ps-0">
+      <div class="p-3 info-horizontal">
+        <div class="icon icon-shape  bg-gradient-primary shadow-primary text-center">
+          <i class="fas fa-ship opacity-10"></i>
+        </div>
+        <div class="description ps-3">
+          <p class="mb-0">It becomes harder for us to give others a hand. <br> We get our heart broken by people we love.</p>
+        </div>
+      </div>
+
+      <div class="p-3 info-horizontal">
+        <div class="icon icon-shape  bg-gradient-primary shadow-primary text-center">
+          <i class="fas fa-handshake opacity-10"></i>
+        </div>
+        <div class="description ps-3">
+          <p class="mb-0">As we live, our hearts turn colder. <br>Cause pain is what we go through as we become older.</p>
+        </div>
+      </div>
+      <div class="p-3 info-horizontal">
+        <div class="icon icon-shape  bg-gradient-primary shadow-primary text-center">
+          <i class="fas fa-hourglass opacity-10"></i>
+        </div>
+        <div class="description ps-3">
+          <p class="mb-0">When we lose family over time. <br> What else could rust the heart more over time? Blackgold.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
+<div class="card text-center">
+      <div class="overflow-hidden position-relative border-radius-lg bg-cover p-3" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/window-desk.jpg')">
+        <span class="mask bg-gradient-dark opacity-6"></span>
+        <div class="card-body position-relative z-index-1 d-flex flex-column mt-5">
+          <p class="text-white font-weight-bolder">User #hashtag in a photo on social media and get $10 for each purchase you make.</p>
+          <a class="text-white text-sm font-weight-bold mb-0 icon-move-right mt-4" href="javascript:;">
+            Read More
+            <i class="material-icons text-sm ms-1 position-relative" aria-hidden="true">arrow_forward</i>
+          </a>
+        </div>
+      </div>
+    </div>
 
+</div>
+
+<!-- -------- END Features w/ icons and text on left & gradient title and text on right -------- -->
+
+  
 @endsection
--- resources/views/layouts/client/app.blade.php
+++ resources/views/layouts/client/app.blade.php
@@ -33,27 +33,29 @@
 
     <!-- Material Kit-->
     <link type="text/css" href="{{ asset('css/material-kit.min.css') }}" rel="stylesheet">
-    <!-- <style>
-        body{
-            max-width: 100%;
+    <style>
+        body {
+            position: relative;
+            height: 100vh;
         }
-    </style> -->
+    </style>
 
 </head>
 
-<body>
+<body id="container">
 
     <!-- Navbar -->
     <div class="container position-sticky z-index-sticky top-0">
         <div class="row">
             <div class="col-12">
-                <nav class="navbar navbar-expand-lg  blur border-radius-xl top-0 z-index-fixed shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+                <nav
+                    class="navbar navbar-expand-lg  blur border-radius-xl top-0 z-index-fixed shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
                     <div class="container-fluid px-0">
                         <a class="navbar-brand font-weight-bolder ms-sm-3"
                             href="https://demos.creative-tim.com/material-kit/index" rel="tooltip"
                             title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">
                             <img src="https://rukunwarga06.files.wordpress.com/2015/08/logo-dki.png" alt="AdminLTE Logo"
-                              width="45" style="opacity: .8" width>&nbspRukun Warga 16 - Cibabat
+                                width="45" style="opacity: .8" width>&nbspRukun Warga 16 - Cibabat
                         </a>
                         <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
@@ -66,13 +68,13 @@
                         </button>
                         <div class="collapse navbar-collapse pt-3 pb-2 py-lg-0 w-100" id="navigation">
                             <ul class="navbar-nav navbar-nav-hover ms-auto">
-                                <li class="nav-item ps-2 ms-lg-auto">
+                                <li class="nav-item ms-lg-auto">
                                     <a class="nav-link nav-link-icon me-2" href="{{ route('client.home') }}">
                                         <i class="fa-solid fa-house opacity-6 me-1 text-sm"></i>
                                         <p class="d-inline text-sm z-index-1 font-weight-bold">Home</p>
                                     </a>
                                 </li>
-                                
+
                                 <li class="nav-item dropdown dropdown-hover ms-lg-auto">
                                     <a class="nav-link ps-2 d-flex cursor-pointer align-items-center"
                                         id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false">
@@ -88,7 +90,7 @@
                                                 class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-1">
                                                 Landing Pages
                                             </h6>
-                                            <a href="{{route('client.profile')}}" class="dropdown-item border-radius-md">
+                                            <a href="" class="dropdown-item border-radius-md">
                                                 <span>About Us</span>
                                             </a>
                                             <a href="./pages/contact-us.html" class="dropdown-item border-radius-md">
@@ -136,7 +138,7 @@
                                 </li>
 
                                 <li class="nav-item ms-lg-auto">
-                                    <a class="nav-link ps-2 nav-link-icon me-2" href="{{ route('client.home') }}">
+                                    <a class="nav-link ps-2 nav-link-icon me-2" href="{{route('client.profile')}}">
                                         <i class="fa-solid fa-building-columns opacity-6 me-1 text-sm"></i>
                                         <p class="d-inline text-sm z-index-1 font-weight-bold">Profil</p>
                                     </a>
@@ -164,177 +166,187 @@
     </main>
 
     <footer class="footer pt-5 mt-5">
-  <div class="container">
-    <div class=" row">
-      <div class="col-md-3 mb-4 ms-auto">
-        <div>
-          <a href="https://www.creative-tim.com/product/material-kit">
-            <i class="fa-solid fa-building-columns mb-3 footer-logo"></i>
-          </a>
-          <h6 class="font-weight-bolder">Rukun Warga 16</h6>
-          <h6 class="font-weight-bolder mb-4">Kelurahan Cibabat</h6>
+        <div class="container">
+            <div class=" row">
+                <div class="col-md-3 mb-4 ms-auto">
+                    <div>
+                        <a href="https://www.creative-tim.com/product/material-kit">
+                            <i class="fa-solid fa-building-columns mb-3 footer-logo"></i>
+                        </a>
+                        <h6 class="font-weight-bolder">Rukun Warga 16</h6>
+                        <h6 class="font-weight-bolder mb-4">Kelurahan Cibabat</h6>
+                    </div>
+                    <div>
+                        <ul class="d-flex flex-row ms-n3 nav">
+                            <li class="nav-item">
+                                <a class="nav-link pe-1" href="https://www.facebook.com/CreativeTim" target="_blank">
+                                    <i class="fab fa-facebook text-lg opacity-8"></i>
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link pe-1" href="https://twitter.com/creativetim" target="_blank">
+                                    <i class="fab fa-twitter text-lg opacity-8"></i>
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link pe-1" href="https://dribbble.com/creativetim" target="_blank">
+                                    <i class="fab fa-dribbble text-lg opacity-8"></i>
+                                </a>
+                            </li>
+
+
+                            <li class="nav-item">
+                                <a class="nav-link pe-1" href="https://github.com/creativetimofficial" target="_blank">
+                                    <i class="fab fa-github text-lg opacity-8"></i>
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link pe-1" href="https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w"
+                                    target="_blank">
+                                    <i class="fab fa-youtube text-lg opacity-8"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+
+
+                <div class="col-md-2 col-sm-6 col-6 mb-4">
+                    <div>
+                        <h6 class="text-sm">Company</h6>
+                        <ul class="flex-column ms-n3 nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.creative-tim.com/presentation" target="_blank">
+                                    About Us
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.creative-tim.com/templates/free" target="_blank">
+                                    Freebies
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.creative-tim.com/templates/premium"
+                                    target="_blank">
+                                    Premium Tools
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.creative-tim.com/blog" target="_blank">
+                                    Blog
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-2 col-sm-6 col-6 mb-4">
+                    <div>
+                        <h6 class="text-sm">Resources</h6>
+                        <ul class="flex-column ms-n3 nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://iradesign.io/" target="_blank">
+                                    Illustrations
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.creative-tim.com/bits" target="_blank">
+                                    Bits & Snippets
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.creative-tim.com/affiliates/new" target="_blank">
+                                    Affiliate Program
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-2 col-sm-6 col-6 mb-4">
+                    <div>
+                        <h6 class="text-sm">Help & Support</h6>
+                        <ul class="flex-column ms-n3 nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.creative-tim.com/contact-us" target="_blank">
+                                    Contact Us
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.creative-tim.com/knowledge-center"
+                                    target="_blank">
+                                    Knowledge Center
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://services.creative-tim.com/?ref=ct-mk2-footer"
+                                    target="_blank">
+                                    Custom Development
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.creative-tim.com/sponsorships" target="_blank">
+                                    Sponsorships
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-2 col-sm-6 col-6 mb-4 me-auto">
+                    <div>
+                        <h6 class="text-sm">Legal</h6>
+                        <ul class="flex-column ms-n3 nav">
+                            <li class="nav-item">
+                                <a class="nav-link"
+                                    href="https://www.creative-tim.com/knowledge-center/terms-of-service"
+                                    target="_blank">
+                                    Terms & Conditions
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.creative-tim.com/knowledge-center/privacy-policy"
+                                    target="_blank">
+                                    Privacy Policy
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.creative-tim.com/license" target="_blank">
+                                    Licenses (EULA)
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <div class="text-center">
+                        <p class="text-dark my-4 text-sm font-weight-normal">
+                            All rights reserved. Copyright © <script>
+                                document.write(new Date().getFullYear())
+                            </script> Material Kit by <a href="https://www.creative-tim.com" target="_blank">Creative
+                                Tim</a>.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
-          <ul class="d-flex flex-row ms-n3 nav">
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="https://www.facebook.com/CreativeTim" target="_blank">
-                <i class="fab fa-facebook text-lg opacity-8"></i>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="https://twitter.com/creativetim" target="_blank">
-                <i class="fab fa-twitter text-lg opacity-8"></i>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="https://dribbble.com/creativetim" target="_blank">
-                <i class="fab fa-dribbble text-lg opacity-8"></i>
-              </a>
-            </li>
-
-
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="https://github.com/creativetimofficial" target="_blank">
-                <i class="fab fa-github text-lg opacity-8"></i>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w" target="_blank">
-                <i class="fab fa-youtube text-lg opacity-8"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-
-
-      <div class="col-md-2 col-sm-6 col-6 mb-4">
-        <div>
-          <h6 class="text-sm">Company</h6>
-          <ul class="flex-column ms-n3 nav">
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/presentation" target="_blank">
-                About Us
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/templates/free" target="_blank">
-                Freebies
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/templates/premium" target="_blank">
-                Premium Tools
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/blog" target="_blank">
-                Blog
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="col-md-2 col-sm-6 col-6 mb-4">
-        <div>
-          <h6 class="text-sm">Resources</h6>
-          <ul class="flex-column ms-n3 nav">
-            <li class="nav-item">
-              <a class="nav-link" href="https://iradesign.io/" target="_blank">
-                Illustrations
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/bits" target="_blank">
-                Bits & Snippets
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/affiliates/new" target="_blank">
-                Affiliate Program
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="col-md-2 col-sm-6 col-6 mb-4">
-        <div>
-          <h6 class="text-sm">Help & Support</h6>
-          <ul class="flex-column ms-n3 nav">
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/contact-us" target="_blank">
-                Contact Us
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/knowledge-center" target="_blank">
-                Knowledge Center
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://services.creative-tim.com/?ref=ct-mk2-footer" target="_blank">
-                Custom Development
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/sponsorships" target="_blank">
-                Sponsorships
-              </a>
-            </li>
-
-          </ul>
-        </div>
-      </div>
-
-      <div class="col-md-2 col-sm-6 col-6 mb-4 me-auto">
-        <div>
-          <h6 class="text-sm">Legal</h6>
-          <ul class="flex-column ms-n3 nav">
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/knowledge-center/terms-of-service" target="_blank">
-                Terms & Conditions
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/knowledge-center/privacy-policy" target="_blank">
-                Privacy Policy
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="https://www.creative-tim.com/license" target="_blank">
-                Licenses (EULA)
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="col-12">
-        <div class="text-center">
-          <p class="text-dark my-4 text-sm font-weight-normal">
-            All rights reserved. Copyright © <script>document.write(new Date().getFullYear())</script> Material Kit by <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
+    </footer>
 
 
     <!--   Core JS Files   -->
@@ -361,6 +373,15 @@
 
     <!-- Control Center for Material Kit parallax effects, scripts for the example pages etc -->
     <script src="{{ asset('js/material-kit.min.js') }}" type="text/javascript"></script>
+
+    <!-- <script type="text/javascript">
+        const ps = new
+        PerfectScrollbar('#container', {
+          wheelSpeed: 2,
+          wheelPropagation: true,
+          minScrollbarLength: 20
+        });
+    </script> -->
 </body>
 
 </html>
