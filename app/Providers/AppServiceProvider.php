b057e6ece2111a0b5fcee062efab1edda4a1c818
alianhakim9 <alianhakim9@gmail.com>
2022-07-03 21:14:15 +0700

    menambahkan schema production saat boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
         Paginator::useBootstrap();
     }
 }
