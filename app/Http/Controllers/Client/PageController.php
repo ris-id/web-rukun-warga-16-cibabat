b7206998b547d4c5282b8646ed677d36c8b6eb94
alianhakim9 <alianhakim9@gmail.com>
2022-07-12 06:07:36 +0700

    test memperbaiki error view

--- app/Http/Controllers/Client/PageController.php
+++ app/Http/Controllers/Client/PageController.php
@@ -24,7 +24,7 @@ class PageController extends Controller
     public function profile()
     {
         $profil = Profil::all();
-        return view('client.pages.profile', ['profil' => $profil]);
+        return view('client.Pages.profile', ['profil' => $profil]);
     }
     public function layanan()
     {
